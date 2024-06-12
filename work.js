74edc7aaf02ab768b392b68677af80cd0392787b
유으뜸 <www.goolge.com>
2024-06-12 11:26:09 +0900

    생성자 함수

--- work.js
+++ work.js
@@ -4,25 +4,72 @@
 //   job: "",
 // };
 
-// class BasicData {
-//   constructor(name, age, job) {
-//     this.name = name;
-//     this.age = age;
-//     this.job = job;
-//   }
-// }
-const basicData = () => {
-  return {
-    name: "유으뜸",
-    age: 0,
-    job: "수강생",
-  };
-};
+// * 생성자 함수 setter로 규격 만들기
+// * 생성자 함수로 객체 생성하기
+// * 생성자 함수 출저를 검사하기 -> instanceof
+// * true -> "문자열 HTML" 리턴
+// * false -> throw new Error()
+class BasicData {
+  constructor(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+  }
+
+  set name(value) {
+    if (typeof value === "string") {
+      this._name = value;
+    } else {
+      throw new Error("이름은 문자열이어야 합니다.");
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set age(value) {
+    if (typeof value === "number") {
+      this._age = value;
+    }
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set job(value) {
+    if (typeof value === "string") {
+      this._job = value;
+    }
+  }
+
+  get job() {
+    return this._job;
+  }
+}
 
 const component = () => {
-  let obj = basicData();
+  let obj = new BasicData("공욱재", 26, "미남");
 
   const decision = () => {
-    return `<html></html>`;
+    if (obj instanceof BasicData) {
+      return `
+      <html>
+      <body>
+      <h1>이름: ${obj.name}</h1>
+      <h2>나이: ${obj.age}</h2>
+      <h3>직업: ${obj.job}</h3>
+      </body>
+      </html>
+      
+      `;
+    } else {
+      console.error("규격 미달");
+    }
   };
+
+  return decision();
 };
+
+console.log(component());
