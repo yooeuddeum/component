5f86c26e694fc3f85d34ab178d0074d3bd08aeb1
유으뜸 <www.goolge.com>
2024-06-12 11:58:57 +0900

    연습

--- work2.js
+++ work2.js
@@ -1,8 +1,31 @@
 const component = () => {
-  let obj = {};
+  let obj = {
+    name: "유으뜸",
+    age: 12,
+    job: "수강생",
+  };
 
   let innerFunc = () => {
-    
+    if (obj.age > 20) {
+      return "여름엔 역시 사이다가 최고";
+    } 
+    else {
+      return "여름엔 역시 요구르트가 최고";
+    }
   };
-  return `some string`;
+
+  let Afunc = () => {
+    if(obj.job === "수강생"){
+      return "Good";
+    }
+    else {
+      return "No";
+    }
+  };
+  
+  return `
+  <h1>${obj.name}</h1>
+  <h2>${innerFunc()}</h2>
+  <h3>${Afunc()}</h3>
+  `;
 };
