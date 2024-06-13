f809b6a713a6260e16fe9f7f6d30f21c6c24d9b2
유으뜸 <www.goolge.com>
2024-06-13 15:27:39 +0900

    연습4

--- constructor.js
+++ constructor.js
@@ -30,42 +30,46 @@
 
 //* -------------------------------------------------------
 
-function tese(test1, test2, test3) {
-  this.test1 = test1;
-  this.test2 = test2;
-  this.test3 = test3;
-}
+// function tese(test1, test2, test3) {
+//   this.test1 = test1;
+//   this.test2 = test2;
+//   this.test3 = test3;
+// }
 
-function tese2(test1, test2, test3) {
-  let obj = {};
-  obj.test1 = test1;
-  obj.test2 = test2;
-  obj.test3 = test3;
-  return obj;
-}
+// function tese2(test1, test2, test3) {
+//   let obj = {};
+//   obj.test1 = test1;
+//   obj.test2 = test2;
+//   obj.test3 = test3;
+//   return obj;
+// }
 
-let test = new tese(4, 5, 6);
-let test2 = new tese2(4, 5, 6);
+// let test = new tese(4, 5, 6);
+// let test2 = new tese2(4, 5, 6);
 
-console.log(tese(1, 2, 3));
-console.log(test);
-console.log(test instanceof tese);
+// console.log(tese(1, 2, 3));
+// console.log(test);
+// console.log(test instanceof tese);
 
-console.log(tese2(1, 2, 3));
-console.log(test2);
-console.log(test2 instanceof tese2);
+// console.log(tese2(1, 2, 3));
+// console.log(test2);
+// console.log(test2 instanceof tese2);
 
-class test3 {
-  constructor(something1, something2, something3, something4) {
-    this.something1 = something1;
-    this.something2 = something2;
-    this.something3 = something3;
-    this.something4 = something4;
-  }
-}
+// class test3 {
+//   constructor(something1, something2, something3, something4) {
+//     this.something1 = something1;
+//     this.something2 = something2;
+//     this.something3 = something3;
+//     this.something4 = something4;
+//   }
+// }
 
-const oh = new test3(1, 2, 3, 4);
-console.log(oh);
-console.log(oh instanceof test3);
+// const oh = new test3(1, 2, 3, 4);
+// console.log(oh);
+// console.log(oh instanceof test3);
 
-console.log(test3(5, 6, 7, 8));
+// console.log(test3(5, 6, 7, 8));
+
+//* -------------------------------------------------------
+
+
